de06a40ebd061c609166c8b0afd818ffbe9885d1
siren0413 <siren0413@gmail.com>
2013-06-17 00:00:32 -0500

    1.1

--- src/com/siren/option/math/Binomial.java
+++ src/com/siren/option/math/Binomial.java
@@ -2,25 +2,29 @@ package com.siren.option.math;
 
 public class Binomial {
 
+	private final static double EPSILON = 0.00001;
+	private final static int ITMAX = 100;
+	
+	
 	public static double price(double s, double x, double t, double r, double v, int steps) {
 
-		double timeStep = t / steps;
-		double t_sqrt = Math.sqrt(t);
-		double sigma2 = v * v;
-		double d1 = ((Math.log(s / x)) + (Math.pow(v, 2) / 2) * t) / (v * Math.sqrt(t));
-		double d2 = d1 - v * Math.sqrt(t);
+		final double timeStep = t / steps;
+		// double t_sqrt = Math.sqrt(t);
+		// double sigma2 = v * v;
+		final double d1 = ((Math.log(s / x)) + (Math.pow(v, 2) / 2) * t) / (v * Math.sqrt(t));
+		final double d2 = d1 - v * Math.sqrt(t);
 
-		double R = Math.exp(r * timeStep);
+		final double R = Math.exp(r * timeStep);
 		double B = Math.exp(-r * timeStep);
 
-		double probUp = peizerPratt2(d2, steps);
-		double probUp1 = peizerPratt2(d1, steps);
-		double probDown = 1 - probUp;
-		double up = R * probUp1 / probUp;
-		double down = (R - probUp * up) / probDown;
+		final double probUp = peizerPratt2(d2, steps);
+		final double probUp1 = peizerPratt2(d1, steps);
+		final double probDown = 1 - probUp;
+		final double up = R * probUp1 / probUp;
+		final double down = (R - probUp * up) / probDown;
 
-		double[] payOffValues = new double[steps + 1];
-		double[] nodePrices = new double[steps + 1];
+		final double[] payOffValues = new double[steps + 1];
+		final double[] nodePrices = new double[steps + 1];
 
 		nodePrices[0] = s * Math.pow(up, steps);
 		payOffValues[0] = payOff(nodePrices[0], x);
@@ -33,7 +37,7 @@ public class Binomial {
 		for (int S = steps - 1; S >= 0; S--) {
 			for (int i = 0; i <= S; i++) {
 				nodePrices[i] = nodePrices[i] / up;
-				double nodepayOff = payOff(nodePrices[i], x);
+				final double nodepayOff = payOff(nodePrices[i], x);
 
 				payOffValues[i] = (payOffValues[i] * probUp + payOffValues[i + 1] * probDown) * B;
 				payOffValues[i] = nodeValue(payOffValues[i], nodepayOff);
@@ -55,6 +59,31 @@ public class Binomial {
 
 	private static double payOff(double s, double x) {
 		return Math.max(0.0, s - x);
+	}
+	
+	// bisection
+	public static double impliedVol2(double p0, String cp, double S, double X, double T, double R) throws Exception {
+		double vol = 0.0;
+		double low = 0.0000001;
+		double high = 5.0;
+		double price = Double.MAX_VALUE;
+		int count = 0;
+		while (Math.abs(p0 - price) > EPSILON) {
+			vol = (high + low) / 2;
+			price = Binomial.price(S, X, T, R, vol, 60);
+			if (price > p0) {
+				high = vol;
+			} else {
+				low = vol;
+			}
+			count++;
+			if (count == ITMAX) {
+				System.err.println("bisection maxout");
+				break;
+			}
+		}
+		// System.err.println("bisection:"+count);
+		return vol;
 	}
 
 }
--- src/com/siren/option/math/TestBinomial.java
+++ src/com/siren/option/math/TestBinomial.java
@@ -1,8 +1,20 @@
 package com.siren.option.math;
 
+import java.util.Random;
+
 public class TestBinomial {
 
 	public static void main(String[] args) throws Exception {
+		double s = 30;
+		double x = 32;
+		double t = 1;
+		double r = 0.05;
+		double price = 3.22;
+		Random random = new Random(System.currentTimeMillis());
+		double[] ss = new double[1000000];
+		for (int i = 0; i < ss.length; i++) {
+			ss[i] = Math.abs(s + (random.nextDouble() - 0.5) * 8.0);
+		}
 		System.out.println("binomial:" + Binomial.price(30, 32, 1, 0.05, 0.3, 60));
 		System.out.println("Black76:" + Black76.price("c", 30, 32, 1, 0.05, 0.3));
 		
@@ -11,7 +23,7 @@ public class TestBinomial {
 		System.out.println("bimonial:");
 		start = System.currentTimeMillis();
 		for(int i = 0 ; i < 100000; i ++) {
-			Binomial.price(30, 32, 1, 0.05, 0.3, 60);
+			Binomial.impliedVol2(price, "c", ss[i], x, t, r);
 		}
 		System.out.println("binomial finish:"+ (System.currentTimeMillis() - start));
 		
@@ -24,6 +36,9 @@ public class TestBinomial {
 			Black76.impliedVol6(3.22, "c", 30, 32, 1, 0.05);
 		}
 		System.out.println("Black76 finish:"+ (System.currentTimeMillis() - start));
+		
+		
+		
 		
 		
 		
